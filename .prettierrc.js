5f61e4c1c1d7d04941dc0b5f9954284c775b02c2
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2026-01-17 18:43:47 -0700

    chore: add prettier configuration

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: false,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "always",
+  printWidth: 120,
+  endOfLine: "auto",
+}
+
+module.exports = config
