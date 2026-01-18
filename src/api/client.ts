62a93b2321f9a3d2d9b387885dd742c000e9217a
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2026-01-17 18:13:16 -0700

    fix: update axios client configuration to use CONFIG.API_URL and remove obsolete MMKV storage

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,8 +1,8 @@
+import { CONFIG } from "@/src/config";
 import axios from "axios";
-import { API_BASE_URL } from "@/src/config";
 
 export const client = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: CONFIG.API_URL,
   headers: {
     "Content-Type": "application/json",
   },
