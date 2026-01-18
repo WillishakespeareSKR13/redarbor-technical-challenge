5c30bfdcc5a03c8d1c9de82874af0f7423310fd2
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2026-01-17 18:10:25 -0700

    chore: setup project configuration and dependencies
    Add package.json with React Native and Expo dependencies
    Configure TypeScript, Babel, and Unistyles
    Add .gitignore and VSCode settings

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+/** @type {import('react-native-worklets/plugin').PluginOptions} */
+
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    env: {
+      production: {
+        plugins: [["transform-remove-console", { exclude: ["error", "warn"] }]],
+      },
+    },
+  };
+};
