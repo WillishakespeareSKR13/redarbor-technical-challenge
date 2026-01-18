3b4bc93de7160ffc5b8103cc680a51397ebad10b
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2026-01-18 15:09:27 -0700

    feat: update main screen to showcase new components

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,38 +1,40 @@
 import { useTheme } from "@/src/services/shared/hooks/theme"
-import { Text } from "@components"
-import { Button, View } from "react-native"
+import { ActionButton, Button, InputText, Text } from "@components"
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6"
+import { View } from "react-native"
+import { GestureHandlerRootView } from "react-native-gesture-handler"
 import { StyleSheetTheme } from "../services/shared/utils/stylesheet"
 
-const ChangeTheme = () => {
-  const theme = useTheme()
-  const styles = useStyles()
-  return (
-    <>
-      <Text>Current Theme: {theme.key}</Text>
-      <Button
-        onPress={() => {
-          theme.toggle?.()
-        }}
-        title="Toggle Theme"
-      />
-    </>
-  )
-}
-
 const Screen = () => {
-  const styles = useStyles()
+  const styles = Style()
+  const theme = useTheme()
   return (
-    <View style={styles.container}>
-      <Text>Redarbor Technical Challenge</Text>
-      <Text>Expo 54 + React Native + TypeScript + Zustand</Text>
-      <ChangeTheme />
-    </View>
+    <GestureHandlerRootView>
+      <View style={styles.container}>
+        <Text fz="h4" ta="center">
+          Redarbor Technical Challenge
+        </Text>
+        <Text fz="h4" ta="center" c="gray.700">
+          Expo 54 + React Native + TypeScript + Zustand
+        </Text>
+        <Text c="gray.700">Current Theme: {theme.key}</Text>
+        <InputText />
+        <ActionButton icon={<FontAwesome6 name="thumbs-up" />} />
+        <Button
+          onPress={() => {
+            theme.toggle?.()
+          }}
+        >
+          Toggle Theme
+        </Button>
+      </View>
+    </GestureHandlerRootView>
   )
 }
 
 export default Screen
 
-const useStyles = StyleSheetTheme((theme) => ({
+const Style = StyleSheetTheme((theme) => ({
   container: {
     flex: 1,
     backgroundColor: theme.colors.background[100],
