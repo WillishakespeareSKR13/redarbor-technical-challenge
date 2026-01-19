fd702a41470cd981ac6fa651e51c3a2a8763fdf4
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2026-01-18 21:57:36 -0700

    refactor: remove Skeleton component and update exports

--- src/services/shared/components/Skeleton/types.ts
+++
@@ -1,9 +0,0 @@
-export type SkeletonProps = {
-  isLoading?: boolean
-  shimmerSpeed?: number
-  shimmerBackgroundColor?: string
-  gradientColors?: [string, string]
-  defaultRadius?: number
-  animationType?: "gradient" | "pulse" | "none"
-  children: React.ReactNode
-}
